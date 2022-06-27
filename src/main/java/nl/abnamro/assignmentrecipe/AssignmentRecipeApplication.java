599ecd6118dad077dd6f55d8a79acfbea30fd73d
Laio Dias <laio.dias@lunatech.nl>
2022-06-27 18:22:28 +0200

    Add spring boot project

---
+++ src/main/java/nl/abnamro/assignmentrecipe/AssignmentRecipeApplication.java
@@ -0,0 +1,13 @@
+package nl.abnamro.assignmentrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/abnamro/assignmentrecipe/AssignmentRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.abnamro.assignmentrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
